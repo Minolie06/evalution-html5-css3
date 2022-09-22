b770c6f23efcc1f186d1fe294531f0cb2a8dd81a
Virginie Morque <v.morque@capitechnic.com>
2022-09-22 20:36:37 +0200

    responsive

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2,3 +2,7 @@
 //todo: portfolio portfolio grid clic
 //todo: portfolio bouton voir plus clic
 //todo: optimisation image (differentes tailles) + chargement
+
+document.getElementById('contact-btn-send').addEventListener('click', () => {
+    document.getElementById('contact-form').submit();
+})
